5a36d6b8d7426feb19ec8540d3f44635f6d669fc
Alex <alex2005.uy@gmail.com>
2019-07-20 21:06:50 -0300

    Se comentan metodos

--- app/src/main/java/com/example/obnativasp1/listaCategorias.java
+++ app/src/main/java/com/example/obnativasp1/listaCategorias.java
@@ -14,12 +14,10 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class listaCategorias extends Activity implements asyncResponse{
+public class listaCategorias extends Activity implements asyncResponse {
 
 
-    ListView listView ;
-
-
+    ListView listView;
 
 
     @Override
@@ -27,10 +25,10 @@ public class listaCategorias extends Activity implements asyncResponse{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ativity_list_view_categorias);
 
-        // Get ListView object from xml
+        //Obtrengo el listview from xml
         listView = (ListView) findViewById(R.id.listaCategorias);
 
-        List<String> parametros= new ArrayList<String>();
+        List<String> parametros = new ArrayList<String>();
         //String url = "http://apiort.montevideo-gh.com/public/categoria/";
         String url = "https://apiort.portalbase.uy/categoria";
         parametros.add(url);
@@ -38,89 +36,77 @@ public class listaCategorias extends Activity implements asyncResponse{
         /* ojo on el orden*/
         String[] paramVector = new String[parametros.size()];
         paramVector = parametros.toArray(paramVector);
-        String resultado= "";
+        String resultado = "";
 
-try {
-     resultado = new restCategorias(this).execute(paramVector).get();
+        try {
+            resultado = new restCategorias(this).execute(paramVector).get();
 
-    JSONObject retorno =  new JSONObject(resultado.substring(resultado.indexOf("{"), resultado.lastIndexOf("}") + 1));
-
-
-    /* Aca tengo todo el array de la dec*/
-    JSONArray params = retorno.getJSONArray("description");
+            JSONObject retorno = new JSONObject(resultado.substring(resultado.indexOf("{"), resultado.lastIndexOf("}") + 1));
 
 
-    List<String> listaCategorias = new ArrayList<>();
+            /* Aca tengo todo el array de la dec , el JSON viene muy entreverado, y dentro de el hay una seccion o un JSON propio
+             * que es el descripcion, ahi dentro estan las pelis con los id y el titulo, dentro de sescripcion hay muchos subjsons uno
+             * por cada pelicula, por eso hacemos un jsonarray y lo buscamod por etiqueta, en este caso description*/
+            JSONArray params = retorno.getJSONArray("description");
 
 
-    for (int i=0; i<params.length();i++)
-    {
-        JSONObject pelicula = params.getJSONObject(i);
-        System.out.println( "el nombre de la peli es "+pelicula.getString("titulo"));
-        listaCategorias.add(pelicula.getString("titulo"));
-    }
+            /* Como la lista recibe un vector de strings creamos una lista*/
+            List<String> listaCategorias = new ArrayList<>();
 
 
-    String[] values = listaCategorias.toArray(new String[0]);
+            /* Iteramos sobre el array de jsons y obtenemos los subjsons*/
+            for (int i = 0; i < params.length(); i++) {
+                JSONObject pelicula = params.getJSONObject(i);
+                //System.out.println( "el nombre de la peli es "+pelicula.getString("titulo"));
+                /* Obtenemos el string que corresponde a la etiqueta titulo y se lo asignamos a la lista*/
+                listaCategorias.add(pelicula.getString("titulo"));
+            }
 
 
-
-/*
-    for (int i = 0; i < keys.length (); ++i) {
-
-        String key = keys.getString (i); // Here's your key
-        String value = lista.getString (key); // Here's your value
-        System.out.println( "el primer valor es "+value);
-        System.out.println( "el primer valor es "+value);
-
-    }
-
-*/
-
-    // Define a new Adapter
-    // First parameter - Context
-    // Second parameter - Layout for the row
-    // Third parameter - ID of the TextView to which the data is written
-    // Forth - the Array of data
-
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_1, android.R.id.text1, values);
+            /* Convertimos el arraylist en un vector*/
+            String[] values = listaCategorias.toArray(new String[0]);
 
 
-    // Assign adapter to ListView
-    listView.setAdapter(adapter);
+            // Definimos un adaptador nuevo
+            // primer  parametro - Contexto
+            // Segundo parametro - Layout for the row
+            // Tercer parametro - ID del TextView donde escribimos
+            // cuarto - los datos
 
-    // ListView Item Click Listener
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view,
-                                int position, long id) {
 
-            // ListView Clicked item index
-            int itemPosition     = position;
+            // Asignamos el adaptador a la listview
+            listView.setAdapter(adapter);
 
-            // ListView Clicked item value
-            String  itemValue    = (String) listView.getItemAtPosition(position);
+            // Agreganos un listener al click de la lista
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            // Show Alert
-            Toast.makeText(getApplicationContext(),
-                    "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-                    .show();
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
 
+                    // ListView Clicked item index
+                    int itemPosition = position;
+
+                    // ListView Clicked item value
+                    String itemValue = (String) listView.getItemAtPosition(position);
+
+                    // Show Alert
+                    Toast.makeText(getApplicationContext(),
+                            "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                            .show();
+
+                }
+
+            });
+
+
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+            ;
         }
-
-    });
-
-
-} catch (Exception error)
-{
-  System.out.println( error.getMessage());;
-}
-
-
-
-
 
 
     }
